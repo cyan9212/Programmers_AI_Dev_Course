e98e05017052408b2795c5f71a2e0a344aaca4be
cyan9212 <42201371+cyan9212@users.noreply.github.com>
2021-12-08 22:06:55 +0900

    Create 연결 리스트 순회.py

---
+++ 1. 자료구조와 알고리즘/Linked_List/연결 리스트 순회.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, item):
+        self.data = item
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.nodeCount = 0
+        self.head = None
+        self.tail = None
+
+    def getAt(self, pos):
+        if pos < 1 or pos > self.nodeCount:
+            return None
+        i = 1
+        curr = self.head
+        while i < pos:
+            curr = curr.next
+            i += 1
+        return curr
+
+    def traverse(self):
+        result = []
+        pointer = self.head
+        while pointer!=None:
+            result.append(pointer.data)
+            pointer = pointer.next
+        return result
