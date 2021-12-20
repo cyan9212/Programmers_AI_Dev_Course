5589460ad138f50e6d9d4b933a84ef7634ac6ffa
cyan9212 <42201371+cyan9212@users.noreply.github.com>
2021-12-20 22:02:06 +0900

    Create Numpy 관계 연산.py

---
+++ 3. Python 데이터 다루기/Numpy/Numpy 관계 연산.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+np.random.seed(42)
+
+A = np.random.randint(0, 100, size=(3, 3, 3))
+
+# A안에 52 또는 1인 요소와 같은 위치에는 True, 다른 곳에는 False인 배열을 result에 넣어주세요.
+result = 
+((A.reshape(-1) == 52) | (A.reshape(-1) == 1)).reshape(3,3,3)
+
+print(result)
