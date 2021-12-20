dd7e53d2da169ce2acc3546affdfa2fc716e6f71
cyan9212 <42201371+cyan9212@users.noreply.github.com>
2021-12-20 22:02:46 +0900

    Create np.matmul() 함수.py

---
+++ 3. Python 데이터 다루기/Numpy/np.matmul() 함수.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+a = np.random.randn(3, 16, 64)
+b = np.random.randn(3, 64, 8)
+
+# np.dot(a, b)를 빈칸에 넣어주세요.
+dot_result = 
+a.dot(b)
+
+# np.matmul(a, b)를 빈칸에 넣어주세요.
+matmul_result = 
+a @ b
