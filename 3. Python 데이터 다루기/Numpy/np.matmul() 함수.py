364b6a8a978d4b501eec9100116f057c391b6c8f
cyan9212 <42201371+cyan9212@users.noreply.github.com>
2021-12-22 00:02:04 +0900

    Update np.matmul() 함수.py

--- 3. Python 데이터 다루기/Numpy/np.matmul() 함수.py
+++ 3. Python 데이터 다루기/Numpy/np.matmul() 함수.py
@@ -4,9 +4,7 @@ a = np.random.randn(3, 16, 64)
 b = np.random.randn(3, 64, 8)
 
 # np.dot(a, b)를 빈칸에 넣어주세요.
-dot_result = 
-a.dot(b)
+dot_result = a.dot(b)
 
 # np.matmul(a, b)를 빈칸에 넣어주세요.
-matmul_result = 
-a @ b
+matmul_result = a @ b
