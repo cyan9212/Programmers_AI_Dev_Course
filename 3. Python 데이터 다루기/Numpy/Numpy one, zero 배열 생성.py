11da156353a9fbf001207b148280dc42e8a5a6cb
cyan9212 <42201371+cyan9212@users.noreply.github.com>
2021-12-22 00:00:24 +0900

    Update Numpy one, zero 배열 생성.py

--- 3. Python 데이터 다루기/Numpy/Numpy one, zero 배열 생성.py
+++ 3. Python 데이터 다루기/Numpy/Numpy one, zero 배열 생성.py
@@ -3,13 +3,11 @@ import numpy as np
 np.random.seed(42)
 
 # np.ones()을 사용하여 2 x 1 x 3 모양을 가진 배열을 만들어줍니다.
-A = 
-np.ones(shape=(2,1,3))
+A = np.ones(shape=(2,1,3))
 
 
 # np.zeros()을 사용하여 3 x 4 x 2 모양을 가진 배열을 만들어줍니다.
-B = 
-np.zeros(shape=(3,4,2))
+B = np.zeros(shape=(3,4,2))
 
 
 print('-------------A의 출력-------------')
