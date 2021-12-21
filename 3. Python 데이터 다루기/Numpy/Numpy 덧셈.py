64fc2bc4a6756d94ec3c78b9b2a98e3968ade68a
cyan9212 <42201371+cyan9212@users.noreply.github.com>
2021-12-22 00:00:54 +0900

    Update Numpy 덧셈.py

--- 3. Python 데이터 다루기/Numpy/Numpy 덧셈.py
+++ 3. Python 데이터 다루기/Numpy/Numpy 덧셈.py
@@ -2,13 +2,11 @@ import numpy as np
 
 arr_A = np.array([[1, 2, 3], [3, 4, 5]])
 # arr_A와 동일한 차원과 모양을 가진 배열을 생성해주세요.
-arr_B = 
-np.array([[1, 2, 3], [3, 4, 5]])
+arr_B = np.array([[1, 2, 3], [3, 4, 5]])
 
 print(arr_B)
 
 # arr_A와 arr_B의 요소별 덧셈 결과를 np_result에 담아주세요.
-np_result = 
-arr_A + arr_B
+np_result = arr_A + arr_B
 
 print(np_result)
